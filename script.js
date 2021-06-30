4b43c944b3f4705f4d1cb4ac479997151a541a50
Ray a <rayeed.ashir@gmail.com>
2021-06-30 15:59:34 -0700

    added capturePasswordLength function

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ function writePassword() {
 }
 
 /*
+  function to capture the length of the password from the user. 
+  Password length must be between 8-128. Fucntion will keep prompting the user
+  until a valid password length is submitted
+  parameters: none
+  return: passwordLength a number between 8-128
+  */
+  function capturePasswordLength() {
+    let passwordLength = prompt('Enter the number of characters');
+    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      alert('Invalid number of characters. Enter 8-128');
+      passwordLength = prompt('Enter the number of characters'); 
+    }
+    return passwordLength;
+  }
+
+  /*
   Random number generator. It will return a value between 0 and max. 
   input: max - the maximum number to be generated
   return: a number between 0 and max
